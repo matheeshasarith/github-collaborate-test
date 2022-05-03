b13bc55b01aa944c00ba643c201dd1a05ab13568
Matheesha Sarith Adhikari <37117921+matheeshasarith@users.noreply.github.com>
2022-05-03 11:51:29 +0530

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -74,6 +74,11 @@
                             <div class="ml-12">
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
                                     Laracasts offers thousands of video tutorials on Laravel, PHP, and JavaScript development. Check them out, see for yourself, and massively level up your development skills in the process.
+                                    <ul>
+                                        <li>matheesha-sarith branch test</li>
+                                        <li>......</li>
+                                        <li>......</li>
+                                    </ul>
                                 </div>
                             </div>
                         </div>
